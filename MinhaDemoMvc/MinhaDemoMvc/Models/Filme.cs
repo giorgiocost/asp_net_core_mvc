e0afe25ec34d35b97a8b2ef7e2850e4710ac37fb
giorgiocost <gegiorgio1991@gmail.com>
2020-10-04 01:52:56 -0300

    model Filme

---
+++ MinhaDemoMvc/MinhaDemoMvc/Models/Filme.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MinhaDemoMvc.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public DateTime DataLancamento { get; set; }
+        public string Genero { get; set; }
+        public Decimal Valor { get; set; }
+        public string Avaliacao { get; set; }
+    }
+}
