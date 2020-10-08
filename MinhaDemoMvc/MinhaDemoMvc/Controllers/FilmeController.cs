2ad7424c218f6b4330af3f11543d68da22ce9195
giorgiocost <gegiorgio1991@gmail.com>
2020-10-07 23:43:24 -0300

    validação formulário back e front

--- MinhaDemoMvc/MinhaDemoMvc/Controllers/FilmeController.cs
+++ MinhaDemoMvc/MinhaDemoMvc/Controllers/FilmeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MinhaDemoMvc.Models;
 
 namespace MinhaDemoMvc.Controllers
 {
@@ -12,6 +13,16 @@ namespace MinhaDemoMvc.Controllers
         public IActionResult Adicionar()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Adicionar(Filme filme)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(filme);
         }
     }
 }
